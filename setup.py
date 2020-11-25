b3f8c9cd910a89a9a27ec57f251e62f248c81e38
Nick Chapman <36717294+nchapman95@users.noreply.github.com>
2020-11-24 18:29:27 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from setuptools import setup
 
+setup(name='pydlbaseball',
+      version='0.1',
+      description='Deep Learning Tools for training baseball models.',
+      url='https://github.com/nchapman95/pydlbaseball/',
+      author='Nick Chapman',
+      author_email='nchapman958@gmail.com',
+      license='MIT',
+      packages=['pydlbaseball'],
+      zip_safe=False)
