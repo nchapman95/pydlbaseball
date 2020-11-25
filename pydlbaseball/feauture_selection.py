dc93b3a1dd475cf13cf116cbe240430bcf1f2468
Nick Chapman <36717294+nchapman95@users.noreply.github.com>
2020-11-24 18:57:46 -0800

    Create feauture_selection.py

---
+++ pydlbaseball/feauture_selection.py
@@ -0,0 +1,35 @@
+def iv_woe(data, target, bins=10, show_woe=False):
+    
+    #Empty Dataframe
+    newDF,woeDF = pd.DataFrame(), pd.DataFrame()
+    
+    #Extract Column Names
+    cols = data.columns
+    
+    #Run WOE and IV on all the independent variables
+    for ivars in cols[~cols.isin([target])]:
+        if (data[ivars].dtype.kind in 'bifc') and (len(np.unique(data[ivars]))>10):
+            binned_x = pd.qcut(data[ivars], bins,  duplicates='drop')
+            d0 = pd.DataFrame({'x': binned_x, 'y': data[target]})
+        else:
+            d0 = pd.DataFrame({'x': data[ivars], 'y': data[target]})
+        d = d0.groupby("x", as_index=False).agg({"y": ["count", "sum"]})
+        d.columns = ['Cutoff', 'N', 'Events']
+        d['% of Events'] = np.maximum(d['Events'], 0.5) / d['Events'].sum()
+        d['Non-Events'] = d['N'] - d['Events']
+        d['% of Non-Events'] = np.maximum(d['Non-Events'], 0.5) / d['Non-Events'].sum()
+        d['WoE'] = np.log(d['% of Events']/d['% of Non-Events'])
+        d['IV'] = (d['WoE'] * (d['% of Events'] - d['% of Non-Events'])).round(4)
+        d.insert(loc=0, column='Variable', value=ivars)
+        #print("Information value of " + ivars + " is " + str(round(d['IV'].sum(),6)))
+        temp =pd.DataFrame({"Variable" : [ivars], "IV" : [d['IV'].sum()]}, columns = ["Variable", "IV"])
+        newDF=pd.concat([newDF,temp], axis=0)
+        woeDF=pd.concat([woeDF,d], axis=0)
+
+        newDF.sort_values("IV",ascending=False, inplace=True)
+        woeDF.sort_values("IV",ascending=False, inplace=True)
+        #Show WOE Table
+        if show_woe == True:
+            print(d)
+    newDF = newDF.rename(columns={"IV":"IV_" + str(target)})
+    return newDF, woeDF
